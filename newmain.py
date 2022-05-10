02b51f2731ead136c599065f4a1bc6a4bda21067
Willmer Wohlén <willmer@wohlen.se>
2022-05-10 14:19:03 +0200

    Utökade svar klara¨

--- newmain.py
+++ newmain.py
@@ -3,6 +3,7 @@ from kivy.properties import ObjectProperty
 from kivy.uix.floatlayout import FloatLayout
 from kivy.core.window import Window
 from kivy.uix.togglebutton import ToggleButton
+from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 from kivy.uix.popup import Popup
 
@@ -26,16 +27,31 @@ class NewResponsee(FloatLayout):
 
     smiley_list = ["mycket_missnojd", "missnojd", "nojd", "mycket_nojd"]
 
+    questions = ["Volym", "För svårt", "Inte roligt", "Något annat"]
+
+    extended_questions = []
+
     selected_smiley = None
 
-    def checked(self):
+    def checked(self,instance):
+        self.add_extended_questions()
+        self.selected_smiley = instance
+        print(instance)
         self.ids["submit"].pos_hint = {"x": 0.425}
-        for i in self.children:
-            try:
-                if i.group == "questions":
-                    i.background_color = (1,1,1,1)
-            except AttributeError:
-                pass
+
+
+
+    def add_extended_questions(self):
+
+
+        spacing = ((0.2, 0.4), (0.6, 0.4), (0.2, 0.2), (0.6, 0.2))
+
+        if self.selected_smiley == None:
+            for i in range(len(self.questions)):
+
+                a = ToggleButton(pos_hint={"x": spacing[i][0], "top": spacing[i][1]}, size_hint=(0.2, 0.05),background_color = (1,1,1,1), group = "questions", text=self.questions[i])
+                self.extended_questions.append(a)
+                self.add_widget(a)
 
     def submit_button(self):
 
@@ -57,13 +73,10 @@ class NewResponsee(FloatLayout):
 
                 self.ids["submit"].pos_hint = {"x": 1}
 
-                for i in self.children:
-                    try:
-                        if i.group == "questions":
-                            i.background_color = (1, 1, 1, 0)
-                            i.state = "normal"
-                    except AttributeError:
-                        pass
+                for question in self.extended_questions:
+                    self.remove_widget(question)
+
+                self.selected_smiley = None
 
 
     def stat_button(self, instance):
@@ -95,23 +108,51 @@ class NewResponsee(FloatLayout):
             plt.show()
 
     def open_questions(self):
-        question_menu()
+        question_terminal.open()
 
 class ChangeQuestion(FloatLayout):
 
-    def save(self):
-        print(self.children)
-    pass
+    def __init__(self, **kwargs):
+        super(ChangeQuestion, self).__init__(**kwargs)
+
+        self.popupWindow = Popup(title="Popup Window", content=self, size_hint=(1, 1))
+
+        self.save_btn = Button(text="save", background_color=(0, 0, 1, 1), size_hint=(0.3, 0.06), pos_hint = {"x": 0.35, "top": 0.1})
+        self.save_btn.bind(on_release=self.save)
+        self.add_widget(self.save_btn)
+
+        self.close_btn = Button(text="close", background_color=(0, 0, 1, 1), size_hint=(0.1, 0.1), pos_hint = {"x": 0.1, "top": 0.1})
+        self.close_btn.bind(on_release=self.close)
+        self.add_widget(self.close_btn)
+        self.questions = []
+
+    def save(self, instance):
+
+        for i in range(len(self.questions)):
+            print(i)
+            myresponsee.questions[i] = self.questions[i].text
 
 
-def question_menu():
-    show = ChangeQuestion()
-
-    popupWindow = Popup(title="Popup Window", content=show, size_hint=(1,1))
 
 
 
-    popupWindow.open()
+
+    def open(self):
+
+        for i in range(len(myresponsee.questions)):
+            spacing = 0.1 + (0.1 * i)
+            a = TextInput(pos_hint={"x": 0.35, "top": 0.3 + spacing}, size_hint=(0.3, 0.06),
+                          text=myresponsee.questions[i])
+            self.questions.append(a)
+            self.add_widget(a)
+
+
+        self.popupWindow.open()
+
+    def close(self, instance):
+        self.popupWindow.dismiss()
+        self.questions = []
+
 
 class ResponseeTwo(App):
     Window.clearcolor = (1, 1, 1, 1)
@@ -120,7 +161,9 @@ class ResponseeTwo(App):
         r = NewResponsee()
         return r
 
-app = ResponseeTwo
 
+app = ResponseeTwo()
+myresponsee = app.build()
+question_terminal = ChangeQuestion()
 if __name__ == "__main__":
-    ResponseeTwo().run()
+    app.run()
